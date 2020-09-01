359e63b7eb4d7c6b89c4ce7efd76b8d889ed77d8
joannatrojak <42413888+joannatrojak@users.noreply.github.com>
2020-09-01 13:51:11 +0200

    fix error

--- app/index.js
+++ app/index.js
@@ -95,7 +95,7 @@ app.post('/emc2/', function (req, res) {
             }
             break; 
         case 'website': 
-        var professor = req.body.queryResult.outputContexts[0].parameters.professor; 
+        var professor = req.body.queryResult.outputContexts; 
         console.log(professor);
         if (professor != null){
             splitProfessor = professor.split(" ");
