ef1b0f929e08140be2431774cc4d76b95ade5f05
joannatrojak <42413888+joannatrojak@users.noreply.github.com>
2020-09-01 13:10:25 +0200

    typo in paramters

--- app/index.js
+++ app/index.js
@@ -75,7 +75,7 @@ app.post('/emc2/', function (req, res) {
             res.send(outString);
             break;
         case 'email': 
-            var professor = req.body.queryResult.outputContext.paramters.professor; 
+            var professor = req.body.queryResult.outputContext.parameters.professor; 
             console.log(professor);
             if (professor != null){
                 splitProfessor = professor.split(" ");
