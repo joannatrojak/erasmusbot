bf18ee4b41b8a3c006fc85e7fc7f083cf7a05daa
joannatrojak <42413888+joannatrojak@users.noreply.github.com>
2020-09-01 13:29:15 +0200

    record one

--- app/index.js
+++ app/index.js
@@ -75,7 +75,7 @@ app.post('/emc2/', function (req, res) {
             res.send(outString);
             break;
         case 'email': 
-            var professor = req.body.queryResult.outputContexts; 
+            var professor = req.body.queryResult.outputContexts[0]; 
             console.log(professor);
             if (professor != null){
                 splitProfessor = professor.split(" ");
