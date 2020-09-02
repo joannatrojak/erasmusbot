12052d4d2ad766f778501ba7a5eeb9cdcc799fd5
joannatrojak <42413888+joannatrojak@users.noreply.github.com>
2020-09-02 10:21:08 +0200

    parameters

--- app/index.js
+++ app/index.js
@@ -95,7 +95,7 @@ app.post('/emc2/', function (req, res) {
             }
             break; 
         case 'website': 
-        var professor = req.body.queryResult.outputContexts[2].parameters.professor; 
+        var professor = req.body.queryResult.outputContexts[2]; 
         console.log(professor);
         if (professor != null){
             splitProfessor = professor.split(" ");
